8f0a3215524d439378cbf48774dc7068e54bd37b
Tom Manner <tsmanner@us.ibm.com>
2019-09-03 12:18:22 -0400

    Implemented generic Parity<D, P> allowing multiple bits of parity to be calculated simultaneously.

--- cpp/Parity/Main.cpp
+++ cpp/Parity/Main.cpp
@@ -13,14 +13,20 @@ using std::setfill;
 
 
 int main() {
-  uint16_t ff = 0xffff;
-  uint16_t fe = 0xfeff;
+  uint8_t x = 0x3e;
+  cout << hex << setw(2) << setfill('0') << static_cast<uint16_t>(x) << " " << static_cast<uint16_t>(Parity<6, 2>::calc(x)) << dec << endl;
 
-  cout << hex << setw(2) << setfill('0') << ff << dec << " " << Parity<14>::calc(ff) << endl;
-  cout << hex << setw(2) << setfill('0') << fe << dec << " " << Parity<14>::calc(fe) << endl;
+  uint8_t ff = 0xff;
+  uint8_t fe = 0xef;
 
-  cout << hex << setw(2) << setfill('0') << ff << dec << " " << Parity<16>::calc(ff) << endl;
-  cout << hex << setw(2) << setfill('0') << fe << dec << " " << Parity<16>::calc(fe) << endl;
+  cout << hex << setw(2) << setfill('0') << static_cast<uint16_t>(ff) << " " << static_cast<uint16_t>(Parity<6>::calc(ff)) << dec << endl;
+  cout << hex << setw(2) << setfill('0') << static_cast<uint16_t>(fe) << " " << static_cast<uint16_t>(Parity<6>::calc(fe)) << dec << endl;
+
+  cout << hex << setw(2) << setfill('0') << static_cast<uint16_t>(ff) << " " << static_cast<uint16_t>(Parity<7>::calc(ff)) << dec << endl;
+  cout << hex << setw(2) << setfill('0') << static_cast<uint16_t>(fe) << " " << static_cast<uint16_t>(Parity<7>::calc(fe)) << dec << endl;
+
+  cout << hex << setw(2) << setfill('0') << static_cast<uint16_t>(ff) << " " << static_cast<uint16_t>(Parity<8>::calc(ff)) << dec << endl;
+  cout << hex << setw(2) << setfill('0') << static_cast<uint16_t>(fe) << " " << static_cast<uint16_t>(Parity<8>::calc(fe)) << dec << endl;
 
   return 0;
 }
--- cpp/Parity/Parity.h
+++ cpp/Parity/Parity.h
@@ -1,33 +1,53 @@
+#include <limits>
 #include <math.h>
 
 
-// TODO: Use the ParityWidth to mask/shift parity bits and control recursion depth
-//       so that a user can calculate parity over multiple equally sized Parity
-//       groups at once.
-//       i.e. Parity<16>::calc(uint16_t(0xfeff))     ->  0b1
-//       i.e. Parity<16, 2>::calc(uint16_t(0xfeff))  ->  0b10
-template <unsigned DataWidth, unsigned ParityWidth = 1, unsigned DataShift = 1>
-struct Parity {
-  // Unsigned precision can be calculated as 1 plus the log base 2 of -1
-  template <typename UnsignedType>
-  static constexpr typename std::enable_if<std::is_unsigned<UnsignedType>::value, unsigned>::type
-  precision() { return std::log2(UnsignedType(-1)) + 1; }
+// To use `enable_if` with a struct, we have to fudge a partial specialization of
+// the template.
+template <unsigned DataWidth, unsigned ParityWidth = 1, unsigned DataShift = 1, typename EnableType = void>
+struct Parity;
+
+// Partial specialization of Parity that enforces ParityWidth being a factor of DataWidth
+template <unsigned DataWidth, unsigned ParityWidth, unsigned DataShift>
+struct Parity<DataWidth, ParityWidth, DataShift, typename std::enable_if<(DataWidth % ParityWidth == 0)>::type> {
+  // static const ParityGroupSize calculation as DataType
+  template <typename DataType> struct ParityGroupSize {
+    static const DataType value = DataWidth / ParityWidth;
+  };
+
+  // static const DataMask calculation as DataType
+  template <typename DataType> struct DataMask {
+    static const DataType value = std::numeric_limits<DataType>::max() >> (std::numeric_limits<DataType>::digits - DataWidth);
+  };
+
+  // static const ParityMask calculation as DataType
+  template <typename DataType> struct ParityMask {
+    static const DataType value = std::numeric_limits<DataType>::max() >> (std::numeric_limits<DataType>::digits - ParityWidth);
+  };
 
   // Masking calc implementation
   //   Input data needs to be masked if the precision of the data type exceeds the
   //   number of bits parity is being calculated over.
   template <typename DataType>
-  static inline typename std::enable_if<(DataWidth < precision<DataType>()), DataType>::type
+  static inline typename std::enable_if<(ParityWidth != 1), DataType>::type
   calc(const DataType& data) {
-    static constexpr DataType mask = DataType(~(DataType(-1) << DataWidth));
-    return _calc(data & mask) & 1;
+    return (
+      (Parity<ParityGroupSize<DataType>::value, ParityWidth-1>::calc(data >> ParityGroupSize<DataType>::value) << 1) |
+       Parity<ParityGroupSize<DataType>::value>::calc(data)
+    ) & ParityMask<DataType>::value;
+  }
+
+  template <typename DataType>
+  static inline typename std::enable_if<(DataWidth < std::numeric_limits<DataType>::digits and ParityWidth == 1), DataType>::type
+  calc(const DataType& data) {
+    return _calc(data & DataMask<DataType>::value) & 1;
   }
 
   // Non-masking calc implementation
   //   Input data does not need to be masked if the precision is equal to the
   //   number of bits parity is being calculated over.
   template <typename DataType>
-  static inline typename std::enable_if<(DataWidth == precision<DataType>()), DataType>::type
+  static inline typename std::enable_if<(DataWidth == std::numeric_limits<DataType>::digits and ParityWidth == 1), DataType>::type
   calc(const DataType& data) {
     return _calc(data) & 1;
   }
@@ -35,14 +55,14 @@ struct Parity {
   // Calculate each step of the parity with a cascading XOR, accumulating
   // partial parity bits in every 2^Nth bit, until 2^N > DataWidth.
   template <typename DataType>
-  static inline typename std::enable_if<(DataWidth > DataShift), DataType>::type
+  static inline typename std::enable_if<((DataWidth / ParityWidth) > DataShift), DataType>::type
   _calc(const DataType& data) {
     return Parity<DataWidth, ParityWidth, DataShift * 2>::_calc(data ^ (data >> DataShift));
   }
 
   // End template recursion
   template <typename DataType>
-  static inline typename std::enable_if<(DataWidth <= DataShift), DataType>::type
+  static inline typename std::enable_if<((DataWidth / ParityWidth) <= DataShift), DataType>::type
   _calc(const DataType& data) { return data; }
 
 };
