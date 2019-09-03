90126da08dbdbc366125aa9f24f46cd01b636c97
Tom Manner <tsmanner@us.ibm.com>
2019-09-03 12:40:36 -0400

    Finished a truly generic implementation of parity X to Y.

--- cpp/Parity/Parity.h
+++ cpp/Parity/Parity.h
@@ -2,41 +2,28 @@
 #include <math.h>
 
 
-// To use `enable_if` with a struct, we have to fudge a partial specialization of
-// the template.
-template <unsigned DataWidth, unsigned ParityWidth = 1, unsigned DataShift = 1, typename EnableType = void>
-struct Parity;
+// To use `enable_if` with a struct, we have to fudge a partial specialization of the template.
+template <unsigned DataWidth, unsigned ParityWidth = 1, unsigned DataShift = 1, typename EnableType = void> struct Parity;
 
 // Partial specialization of Parity that enforces ParityWidth being a factor of DataWidth
 template <unsigned DataWidth, unsigned ParityWidth, unsigned DataShift>
 struct Parity<DataWidth, ParityWidth, DataShift, typename std::enable_if<(DataWidth % ParityWidth == 0)>::type> {
   // static const ParityGroupSize calculation as DataType
-  template <typename DataType> struct ParityGroupSize {
-    static const DataType value = DataWidth / ParityWidth;
-  };
-
+  template <typename DataType> struct ParityGroupSize { static const DataType value = DataWidth / ParityWidth; };
   // static const DataMask calculation as DataType
-  template <typename DataType> struct DataMask {
-    static const DataType value = std::numeric_limits<DataType>::max() >> (std::numeric_limits<DataType>::digits - DataWidth);
-  };
+  template <typename DataType> struct DataMask { static const DataType value = std::numeric_limits<DataType>::max() >> (std::numeric_limits<DataType>::digits - DataWidth); };
 
-  // static const ParityMask calculation as DataType
-  template <typename DataType> struct ParityMask {
-    static const DataType value = std::numeric_limits<DataType>::max() >> (std::numeric_limits<DataType>::digits - ParityWidth);
-  };
+  // Multi-bit parity implementation. Uses template recursion to eventually call single bit
+  // parity on each parity group, ORing in other parity bits shifted left 1 at a time.
+  template <typename DataType>
+  static inline typename std::enable_if<(ParityWidth > 1), DataType>::type
+  calc(const DataType& data) {
+    return (Parity<DataWidth-ParityGroupSize<DataType>::value, ParityWidth-1>::calc(data >> ParityGroupSize<DataType>::value) << 1) | Parity<ParityGroupSize<DataType>::value>::calc(data);
+  }
 
   // Masking calc implementation
   //   Input data needs to be masked if the precision of the data type exceeds the
   //   number of bits parity is being calculated over.
-  template <typename DataType>
-  static inline typename std::enable_if<(ParityWidth != 1), DataType>::type
-  calc(const DataType& data) {
-    return (
-      (Parity<ParityGroupSize<DataType>::value, ParityWidth-1>::calc(data >> ParityGroupSize<DataType>::value) << 1) |
-       Parity<ParityGroupSize<DataType>::value>::calc(data)
-    ) & ParityMask<DataType>::value;
-  }
-
   template <typename DataType>
   static inline typename std::enable_if<(DataWidth < std::numeric_limits<DataType>::digits and ParityWidth == 1), DataType>::type
   calc(const DataType& data) {
