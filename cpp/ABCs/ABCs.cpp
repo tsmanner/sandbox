6ff51b30f4530bf62de022d26a10b1563125e222
Tom Manner <tsmanner@us.ibm.com>
2019-10-09 10:09:40 -0400

    Added a second method to ABCs scratch.

--- cpp/ABCs/ABCs.cpp
+++ cpp/ABCs/ABCs.cpp
@@ -5,12 +5,15 @@
 class MyBaseClass {
 public:
   virtual int foo() = 0;
+  int bar() { return 0xb; }
 };
 
 
 class MyDerivedClass : public MyBaseClass {
 public:
   int foo() { return 5; }
+  int bar() { return 0; }
+
 };
 
 
@@ -21,8 +24,9 @@ public:
 
 
 int main() {
-  MyBaseClass* mdc = new MyDerivedDerivedClass();
+  auto mdc = new MyDerivedClass();
 
   std::cout << mdc << " : " << mdc->foo() << std::endl;
+  std::cout << mdc << " : " << mdc->bar() << std::endl;
 
 }
