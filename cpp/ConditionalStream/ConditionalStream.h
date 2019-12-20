3a85d989e13ee14e36b5d87293384d20140b5320
Tom Manner <tsmanner@us.ibm.com>
2019-12-20 13:17:22 -0500

    Added StreamType template parameter to ConditionalStream so it can be used with any stream, not just std::ostream.

--- cpp/ConditionalStream/ConditionalStream.h
+++ cpp/ConditionalStream/ConditionalStream.h
@@ -5,21 +5,22 @@
 #include <type_traits>
 
 
+template <typename StreamType>
 class ConditionalStream {
 public:
-  ConditionalStream(std::ostream& os): mStream(os), mEnabled(true) {}
+  ConditionalStream(StreamType& inStream): mStream(inStream), mEnabled(true) {}
 
   bool enabled() const { return mEnabled; }
   void setEnabled(const bool& inEnabled) { mEnabled = inEnabled; }
 
   template <typename Element, typename... Elements>
-  typename std::enable_if<(sizeof...(Elements) == 0), std::ostream&>::type
+  typename std::enable_if<(sizeof...(Elements) == 0), StreamType&>::type
   _stream(const Element& element, const Elements&... elements) const {
     return mStream << element;
   }
 
   template <typename Element, typename... Elements>
-  typename std::enable_if<(sizeof...(Elements) != 0), std::ostream&>::type
+  typename std::enable_if<(sizeof...(Elements) != 0), StreamType&>::type
   _stream(const Element& element, const Elements&... elements) const {
     mStream << element;
     _stream<Elements...>(elements...);
@@ -27,21 +28,21 @@ public:
   }
 
   template <typename... Elements>
-  std::ostream& stream(const Elements&... elements) const {
+  StreamType& stream(const Elements&... elements) const {
     if (enabled()) _stream(elements...);
     return mStream;
   }
 
   struct EndlineProxy {
-    friend std::ostream& operator<<(std::ostream& os, const ConditionalStream::EndlineProxy& inEndl) {
-      return os << std::endl;
+    friend StreamType& operator<<(StreamType& inStream, const ConditionalStream::EndlineProxy& inEndl) {
+      return inStream << std::endl;
     }
   };
 
   const EndlineProxy& endl{};
 
 private:
-  std::ostream& mStream;
+  StreamType& mStream;
   bool mEnabled;
 
 };
--- cpp/ConditionalStream/Main.cpp
+++ cpp/ConditionalStream/Main.cpp
@@ -8,7 +8,7 @@ using std::endl;
 
 int main() {
   int x = 5;
-  auto cs = ConditionalStream(cout);
+  auto cs = ConditionalStream<std::ostream>(cout);
   cout << "CS(" << cs.enabled() << "): ";
   cs.stream(1, " hello world ", x, cs.endl);
   cs.setEnabled(false);
